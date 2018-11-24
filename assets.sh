09e370e19a1d1a39226f0cf34b49a84093ac36d1
Joseph Gill <joseph@Josephs-MacBook-Pro.local>
2018-11-23 22:17:34 -0500

    update

---
+++ assets.sh
@@ -0,0 +1,28 @@
+
+# MIGHT use this - don't know
+
+#################  HEAD  ####################
+
+# run this as `sudo -i`
+
+# remember the directory of this file when executed
+THISDIR="$(dirname ${BASH_SOURCE[0]})"
+cd $THISDIR
+
+#################  SETUP  ####################
+
+ASSETSREPONAME=assets.josephgill.io
+ASSETSREPOURL=https://github.com/atljoseph/$ASSETSREPONAME.git
+
+cd ..
+pwd
+if ! [[ -d $ASSETSREPONAME ]]
+then
+    echo "Cloning assets repo"
+    git clone $ASSETSREPOURL
+else 
+    echo "Assets repo already installed"
+fi
+sh $ASSETSREPONAME/scripts/assets-all.sh
+
+cd $THISDIR
