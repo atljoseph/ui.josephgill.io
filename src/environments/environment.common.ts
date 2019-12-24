33d12ef804bdae3f750a87f1731fdf6b78341c97
Joseph Gill <joseph@Josephs-MacBook-Pro.local>
2019-12-24 00:31:00 -0500

    version

--- src/environments/environment.common.ts
+++ src/environments/environment.common.ts
@@ -1,4 +1,4 @@
-export const version = '1.4.3'
+export const version = '1.4.4'
 // export const hiddenRoutes: string[] = [];
 export const assetBaseURL = 'https://s3.us-east-2.amazonaws.com/public.josephgill.io'
 // export const assetBaseURL = '../../../assets/images/resized';
