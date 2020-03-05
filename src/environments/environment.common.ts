00fca47e83b164669369aeb1bc6daa09f038753e
Joseph Gill <joseph@Josephs-MacBook-Pro.local>
2020-03-05 00:16:33 -0500

    bump version

--- src/environments/environment.common.ts
+++ src/environments/environment.common.ts
@@ -1,4 +1,4 @@
-export const version = '1.5.0'
+export const version = '1.5.1'
 // export const hiddenRoutes: string[] = [];
 export const assetProcessedPath = 'processed'
 export const assetBaseURL = 'https://s3.us-east-2.amazonaws.com/public.josephgill.io'
